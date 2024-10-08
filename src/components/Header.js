b7dd446740fb57379de60ac0bbb0d2896972c6f7
Daksh Pratap Singh <daksh6666singh@gmail.com>
2024-10-09 01:28:54 +0530

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -131,7 +131,7 @@ const Header = () => {
           </div>
         </div>
         <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-          <Link to={"https://github.com/thecodophile"}>
+          <Link to={"https://github.com/Daksh4568"}>
             <img
               src="https://avatars.githubusercontent.com/u/108585532?v=4"
               alt="Codophile"
